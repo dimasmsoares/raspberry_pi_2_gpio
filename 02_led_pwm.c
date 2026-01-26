a429e5e7c00434a19b19d0dbd6c18e0c9dca80b8
dimasmsoares <35304959+dimasmsoares@users.noreply.github.com>
2026-01-26 16:36:51 +0000

    Desafio 2 em construção

---
+++ 02_led_pwm.c
@@ -0,0 +1,133 @@
+/*DESAFIO 2 — LED com brilho controlado por PWM*/
+#include <gpiod.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+volatile sig_atomic_t running = 1;  // Ver exlicação ao final sobre o SIGINT
+
+void handle_sigint(int sig)
+{
+    (void)sig;   // evita warning
+    running = 0;
+}
+
+int main(){
+
+    signal(SIGINT, handle_sigint);
+
+    /*STRUCT*/
+    struct gpiod_chip *chip;
+    struct gpiod_request_config *req_cfg;
+    struct gpiod_line_settings *led_line_sets;
+    struct gpiod_line_settings *btn_line_sets;
+    struct gpiod_line_config *line_cfg;
+    struct gpiod_line_request *line_req;
+
+    /*OFFSET*/
+    const unsigned int led_offset[1] = {27};
+    const unsigned int btn_offset[1] = {17};
+
+    /*CHIP*/
+    chip = gpiod_chip_open("/dev/gpiochip0");
+    if(chip == NULL){
+        perror("FUNCTION 'gpiod_chip_open' RETURN NULL");
+        return 1;
+    }
+    printf("CHIP ✅\n");
+    
+    /*REQUEST CONFIG*/
+    req_cfg = gpiod_request_config_new();
+    if(req_cfg == NULL){
+        perror("FUNCTION 'gpiod_request_config_new' RETURN NULL");
+        gpiod_chip_close(chip);    
+        return 1;
+    }
+    gpiod_request_config_set_consumer(req_cfg, "led_ctrl_btn");
+    printf("REQUEST CONFIG ✅\n");
+
+    /*LED -> LINE SETTINGS*/
+    led_line_sets = gpiod_line_settings_new();
+    if(led_line_sets == NULL){
+        perror("FUNCTION 'gpiod_line_settings_new' RETURN NULL (LED)");
+        gpiod_request_config_free(req_cfg);    
+        gpiod_chip_close(chip);    
+        return 1;
+    }
+    gpiod_line_settings_set_direction(led_line_sets, GPIOD_LINE_DIRECTION_OUTPUT);
+    gpiod_line_settings_set_output_value(led_line_sets, GPIOD_LINE_VALUE_INACTIVE);
+    printf("LED -> LINE SETTINGS ✅\n");
+
+    /*BTN -> LINE SETTINGS*/
+    btn_line_sets = gpiod_line_settings_new();
+    if(btn_line_sets == NULL){
+        perror("FUNCTION 'gpiod_line_settings_new' RETURN NULL (BTN)");
+        gpiod_line_settings_free(led_line_sets);
+        gpiod_request_config_free(req_cfg);    
+        gpiod_chip_close(chip);    
+        return 1;
+    }
+    gpiod_line_settings_set_direction(btn_line_sets, GPIOD_LINE_DIRECTION_INPUT);
+    gpiod_line_settings_set_bias(btn_line_sets, GPIOD_LINE_BIAS_PULL_UP);
+    printf("BTN -> LINE SETTINGS ✅\n");
+
+    /*LINE CONFIG*/
+    line_cfg = gpiod_line_config_new();
+    if(line_cfg == NULL){
+        perror("FUNCTION 'gpiod_line_config_new' RETURN NULL");
+        gpiod_line_settings_free(btn_line_sets);    
+        gpiod_line_settings_free(led_line_sets);
+        gpiod_request_config_free(req_cfg);    
+        gpiod_chip_close(chip);    
+        return 1;
+    }
+    gpiod_line_config_add_line_settings(line_cfg, led_offset, 1, led_line_sets);
+    gpiod_line_config_add_line_settings(line_cfg, btn_offset, 1, btn_line_sets);
+    printf("LINE CONFIG ✅\n");
+
+    /*LINE REQUEST*/
+    line_req = gpiod_chip_request_lines(chip, req_cfg, line_cfg);
+    if(line_req == NULL){
+        perror("FUNCTION 'gpiod_chip_request_lines' RETURN NULL");
+        gpiod_line_config_free(line_cfg);
+        gpiod_line_settings_free(btn_line_sets);
+        gpiod_line_settings_free(led_line_sets);
+        gpiod_request_config_free(req_cfg);
+        gpiod_chip_close(chip);
+        return 1;
+    }
+    printf("LINE REQUEST ✅\n");
+
+    printf("WHILE 🔄\n");
+    
+    unsigned int periodo = 10000;
+    unsigned int tempo_on = 0;
+    int led_state = 0;
+    int btn_anterior = 0;
+    int btn_atual = 0;
+
+    while(running){
+        
+    }
+
+
+    /*FREE*/
+    gpiod_line_request_release(line_req);
+    gpiod_line_config_free(line_cfg);
+    gpiod_line_settings_free(btn_line_sets);
+    gpiod_line_settings_free(led_line_sets);
+    gpiod_request_config_free(req_cfg);
+    gpiod_chip_close(chip);
+    return 0;
+}
+
+/* 
+SIGINT --> Signal Interrupt
+    É um sinal (signal) enviado pelo kernel para o processo em execução para interrompe-lo de forma controlada.
+
+    Para usa-lo se registra um handler. Algo como dizer: “Quando chegar um SIGINT, execute esta função.”
+    A função a ser executada muda o estado da flag (sinal).
+
+    SIGINT é o sinal que o Linux envia ao seu programa quando você pressiona Ctrl+C, permitindo que ele encerre de forma limpa.
+
+*/
